e2d824056a342c531114cff9d6c406443fbf2c85
Ian Scott <chesnekov@gmail.com>
2020-08-21 09:38:19 -0700

    add Cat and Dog models/class, both inhereit from the Animal abstract class

---
+++ AnimalShelterAPI/Models/Cat.cs
@@ -0,0 +1,7 @@
+namespace AnimalShelterAPI.Models
+{
+  public class Cat : Animal
+  {
+    public int CatId { get; set; }
+  }
+}
---
+++ AnimalShelterAPI/Models/Dog.cs
@@ -0,0 +1,7 @@
+namespace AnimalShelterAPI.Models
+{
+  public class Dog : Animal
+  {
+    public int DogId { get; set; }
+  }
+}
